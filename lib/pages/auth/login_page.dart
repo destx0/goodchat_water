1f456f7cf88f4ebf0b7f8b96b3b2db29347c012c
destx0 <destisdbest@gmail.com>
2023-01-08 22:22:34 +0530

    login and register ui

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:goodchat_water/pages/auth/register_page.dart';
 import 'package:goodchat_water/widgets/widgets.dart';
 
 class LoginPage extends StatefulWidget {
@@ -10,6 +12,9 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final formKey = GlobalKey<FormState>();
+  String email = "";
+  String password = "";
+  bool _isLoading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,8 +28,13 @@ class _LoginPageState extends State<LoginPage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const Text(
-                "ChatWater",
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                "Echos",
+                style: TextStyle(
+                  fontFamily: 'Fredoka One',
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff395B64),
+                ),
               ),
               const SizedBox(height: 10),
               const Text(
@@ -40,9 +50,23 @@ class _LoginPageState extends State<LoginPage> {
                     color: Theme.of(context).primaryColor,
                   ),
                 ),
+                onChanged: (val) {
+                  setState(() {
+                    var email = val;
+                  });
+                },
+                // check email validation
+                validator: (val) {
+                  return RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(val!)
+                      ? null
+                      : "Please enter a valid email";
+                },
               ),
               const SizedBox(height: 15),
               TextFormField(
+                obscureText: true,
                 decoration: textInputDecoration.copyWith(
                   labelText: "Password",
                   prefixIcon: Icon(
@@ -50,7 +74,58 @@ class _LoginPageState extends State<LoginPage> {
                     color: Theme.of(context).primaryColor,
                   ),
                 ),
+                validator: (val) {
+                  if (val!.length < 6) {
+                    return "Password must be at least 6 characters";
+                  } else {
+                    return null;
+                  }
+                },
+                onChanged: (val) {
+                  setState(() {
+                    var password = val;
+                  });
+                },
               ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 55,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7))),
+                  child: const Text(
+                    "Sign In",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  onPressed: () {
+                    // login();
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text.rich(TextSpan(
+                text: "Don't have an account? ",
+                style: const TextStyle(color: Colors.black, fontSize: 14),
+                children: <TextSpan>[
+                  TextSpan(
+                      text: "Register here",
+                      style: const TextStyle(
+                          color: Colors.black,
+                          decoration: TextDecoration.underline),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          nextScreen(context, const RegisterPage());
+                        }),
+                ],
+              )),
             ],
           ),
         ),
